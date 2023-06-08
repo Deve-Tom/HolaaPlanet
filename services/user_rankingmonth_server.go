5d6c0ee311b44b2ad80467ed33b1304aef8cf9d0
recoding123 <106855017+recoding123@users.noreply.github.com>
2023-06-08 08:55:24 +0800

    排行榜服务

--- services/user_rankingmonth_server.go
+++ services/user_rankingmonth_server.go
@@ -1,1 +1,40 @@
 package services
+
+import (
+	"HolaaPlanet/configs"
+	"HolaaPlanet/entity"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
+
+func Ranking_month(ctx *gin.Context) {
+	User, _ := strconv.Atoi(ctx.Query("user_id"))
+	UserMsg := entity.User{}
+
+	configs.DB.Model(&entity.User{}).Where("user_id = ?", User).First(&UserMsg)
+	if UserMsg.UserID == 0 {
+		ctx.JSON(http.StatusOK, gin.H{
+			"status_code": -1,
+			"status_msg":  "不存在该用户",
+			"ranking":     nil,
+		})
+		return
+	}
+	var count int64 = 0
+	configs.DB.Model(&entity.User{}).Where("month_focus_time > ?", UserMsg.MonthFocusTime).Count(&count)
+	count1 := strconv.FormatInt(count, 10)
+	if count < 100 && count > 0 {
+		ctx.JSON(http.StatusOK, gin.H{
+			"status_code": 0,
+			"status_msg":  "查找成功",
+			"ranking":     count1,
+		})
+	} else {
+		ctx.JSON(http.StatusOK, gin.H{
+			"status_code": 0,
+			"status_msg":  "查找成功",
+			"ranking":     "99+",
+		})
+	}
+}
