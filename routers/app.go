2a73cf6fe768ba5c73e877d2244b4bb74d143f1d
Straaawberry <834220572@qq.com>
2023-06-09 12:02:13 +0800

    由于数据库时长类型修改的用户个人专注时长接口更新，以及用户信息修改接口

--- routers/app.go
+++ routers/app.go
@@ -23,7 +23,7 @@ func InitRouter() *gin.Engine {
 	apiRouter.GET("/user/", middleware.AuthMiddleWare(), services.UserSingleInfoServer)
 
 	//Maintainers:邵洁  Times:2023-06-08
-	apiRouter.PUT("/user/personal_duration/", services.PerDuration)
+	apiRouter.PUT("/user/personal_duration/", services.UpdateInformation)
 
 	return r
 }
